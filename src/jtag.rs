5321c89ab597061faff7f226d648b6617d2f9164
Berkus Decker <berkus@gmail.com>
2025-03-06 21:58:02 +0200

    wip: tms_sequence() now part of JTAG trait to drive SM

--- src/jtag.rs
+++ src/jtag.rs
@@ -19,7 +19,8 @@ pub trait Jtag<DEPS>: From<DEPS> {
 
     fn configure_taps(&mut self, req: &[u8]) -> Result<(), Self::Error>;
 
-    // TODO: What is missing for the 2 other JTAG commands
+    /// Drive state machine to requested state by feeding in a TMS bit sequence.
+    fn tms_sequence(&mut self, data: &[u8], nbits: usize);
 
     fn taps(&mut self) -> &Taps; // to access the TAP sm inside JTAG state?
 }
